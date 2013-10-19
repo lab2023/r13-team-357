a26c2e54e7f5e9c2ded17289d3243a562e8df282
Muhammet DİLEK <dilekmuhammet@gmail.com>
2013-10-19 08:32:57 +0300

    #1 Project created with cybele

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ganban::Application.config.secret_key_base = '02827a0cf01718d26f00273b5ec0e5aa53872a4683be03e15cc42a848e175f175bb360077fbe2b004e2214933b02da277c532c5b34e6b2cba25fbd84151a7220'
