5aa0f2f66bc53fd87721c8e8f6a5e67ea4aa83c1
Tayfun Öziş ERİKAN <tayfun.ozis.erikan@lab2023.com>
2013-10-19 11:13:17 +0300

    #5 Welcome page design was completed

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
+  skip_before_action :authenticate_user!
   def index
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  def title(page_title)
+    content_for(:title) { page_title }
+  end
+
   def avatar_url(user)
     if user
       gravatar_id = Digest::MD5::hexdigest(user.email).downcase
