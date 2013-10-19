9c89de417c81d1bc535a1e5afa4cb6d7271f1148
Muhammet DİLEK <dilekmuhammet@gmail.com>
2013-10-19 14:01:32 +0300

    #3 description added to project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -55,6 +55,6 @@ class ProjectsController < ApplicationController
   end
 
   def project_params
-    params.require(:project).permit(:name)
+    params.require(:project).permit(:name, :description)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131019081653) do
+ActiveRecord::Schema.define(version: 20131019105702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20131019081653) do
     t.integer  "owner_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "description"
   end
 
   add_index "projects", ["owner_id"], name: "index_projects_on_owner_id", using: :btree
